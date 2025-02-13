a93146e9c3b23ff6a4556f67ac90198ec2c8d5bd
Angel75Git <aduboncenteno@usf.edu>
2025-02-13 16:01:01 -0500

    Task 5 and 6 done

---
+++ cc_example.js
@@ -0,0 +1,36 @@
+//Task 5
+// let orders = [
+//     {id: 101, customer: "Alice", total:300},
+//     {id: 102, customer: "Bob", total:450},
+//     {id: 103, customer: "Charlie", total:200},
+// ]
+
+// function findOrder(orders, orderID){
+//     return orders.find(order => order.id === orderID)
+
+// }
+// console.log(findOrder(orders, 102));
+ 
+//Task 6
+
+// let inventroy = {
+//     items: [],
+//     addItem(name, quantity) {
+//         this.items.push({name, quantity});
+//     },
+//     removeLatestItem(){
+//         this.items.pop();
+//     },
+//     removeFirstItem(){
+//         this.items.shift();
+//     },
+// }
+// inventroy.addItem("Monitor", 5);
+// inventroy.addItem("Keybord", 10);
+// inventroy.addItem("Mouse", 3);
+
+// console.log(JSON.stringify(inventroy.items));
+// inventroy.removeLatestItem();
+// console.log(JSON.stringify(inventroy.items));
+// inventroy.removeFirstItem();
+// console.log(JSON.stringify(inventroy.items));
